f076f16d507b212871bd1454f7aaf90c2eb35a61
Nouran Mostafa <nouranmostafa91@gmail.com>
2026-01-05 15:48:30 +0100

    loading packges

---
+++ loading packages.R
@@ -0,0 +1,8 @@
+# Clear the R environment
+rm(list = ls())
+
+# Load required libraries
+library(shiny)      # Web application framework
+library(ggplot2)    # Data visualization
+library(dplyr)      # Data manipulation
+library(reshape2)   # Data reshaping for heatmap
